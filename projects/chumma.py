2d57efde4718c61fc341ab735466889c8b56bb33
swaminathan007 <swaminathang12b2svv@gmail.com>
2023-06-11 09:47:57 +0530

    commit

--- projects/chumma.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image
-
-def get_num_pixels(filepath):
-    width, height = Image.open(filepath).size
-    return (width,height)
-print(get_num_pixels("C:/Users/swami/portfolio/projects/chumma.png"))
